cf85987cba57c86bc962e18e4b7046f67aeff7aa
pooventhira <pooventhira9652@gmail.com>
2025-07-26 21:09:08 +0530

    added daisyUI and forest theme for the app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import NoteDetailPage from './pages/NoteDetailPage'
 
 const App = () => {
   return (
-    <div>
+    <div data-theme='forest'>
       <Routes>
         <Route path='/' element={<HomePage/>} />
         <Route path='/create' element={<CreatePage/>} />
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,3 +13,23 @@ createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </StrictMode>
 )
+
+
+// terminal -> npm create vite@latest
+//    *) creates a rect app using vite
+
+// terminal -> npm i react-router react-hot-toast
+//    *) react-router  --->  for routing
+//    *) react-hot-toast  --->  for notification
+
+// terminal -> npm install -D tailwindcss@3 postcss autoprefixer
+//    *) tailwindcss  --->  for stylesheet free styling
+//    *) postcss  --->  tool for tailwindcss for transforming CSS with JavaScript plugins
+//    *) autoprefixer  --->  PostCSS plugin adds vendor prefixes (-webkit-, -moz-, etc.) ensuring better browser compatibility
+
+// terminal -> npx tailwindcss init -p
+//    *) init  --->  initializes tailwindcss with 'tailwind.config.js'
+//    *) -p  --->  specifically creates 'postcss.config.js' adds tailwindcss and autoprefixer plugins
+
+// terminal -> npm install daisyui
+//    *) daisyui  --->  faster, cleaner, easier tailwindcss development components
