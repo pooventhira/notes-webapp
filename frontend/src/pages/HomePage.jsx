304740d204fb4b291fe5867ee5c08057d4a3c4de
pooventhira <pooventhira9652@gmail.com>
2025-08-07 19:35:30 +0530

    Added NoteCard.jsx component, utils.js for date formating and corrected theme

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import toast from "react-hot-toast";
 
 import Navbar from "../components/NavBar";
 import RateLimitedUI from "../components/RateLimitedUI";
+import NoteCard from "../components/NoteCard";
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
@@ -34,11 +35,24 @@ const HomePage = () => {
   }, []);
   
   return (
-    <div>
-      <Navbar/>
-      {isRateLimited && <RateLimitedUI/>}
+    <div className="min-h-screen">
+      <Navbar />
+
+      {isRateLimited && <RateLimitedUI />}
+
+      <div className="max-w-7xl mx-auto p-4 mt-6">
+        {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
+
+        {notes.length > 0 && !isRateLimited && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {notes.map((note) => (
+              <NoteCard key={note._id} note={note}/>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
 export default HomePage
